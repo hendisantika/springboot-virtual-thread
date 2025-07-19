02a23f9c2bd7935f5940215d4063a4ca137d2009
hendisantika <hendisantika@yahoo.co.id>
2025-07-20 06:39:59 +0700

    feat: Add User Entity🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/virtualthread/entity/User.java
@@ -0,0 +1,36 @@
+package id.my.hendisantika.virtualthread.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : virtual-thread
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 20/07/25
+ * Time: 06.39
+ * To change this template use File | Settings | File Templates.
+ */
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    String email;
+    Long age;
+}
