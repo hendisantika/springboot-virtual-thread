02bd126f9c37e3cac1f6e62f9d2aa3fc5c74427c
hendisantika <hendisantika@yahoo.co.id>
2025-07-20 05:30:59 +0700

    feat: Add generateReportForRegion in PlatformReportService🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/virtualthread/service/PlatformReportService.java
+++ src/main/java/id/my/hendisantika/virtualthread/service/PlatformReportService.java
@@ -1,10 +1,12 @@
 package id.my.hendisantika.virtualthread.service;
 
+import id.my.hendisantika.virtualthread.entity.Customer;
 import id.my.hendisantika.virtualthread.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -28,4 +30,18 @@ public class PlatformReportService {
 
 
     private final Executor executor = Executors.newFixedThreadPool(5);
+
+    public void generateReportForRegion(String region) {
+        executor.execute(() -> {
+            log.info("Platform generating report for region: {} | {}", region, Thread.currentThread());
+
+            List<Customer> customers = repository.findByRegion(region);//1
+            try {
+                CsvReportUtil.writeCustomersToCsv("platform_" + region, customers);//2
+            } catch (Exception e) {
+                System.out.println("❌ Platform Error writing report for region: " + region);
+            }
+        });
+
+    }
 }
